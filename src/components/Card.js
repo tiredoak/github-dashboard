2fa56f21d2ad0e908bd5ae0183a16dc9c7037c8d
Miguel Carvalho <miguelc@tuta.io>
2022-03-23 14:32:48 +0000

    Displaying user information

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,50 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          {twitter_username ? <p>@{twitter_username}</p> : null}
+        </div>
+        <a href={html_url}>Follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className='links'>
+        {company ? (
+          <p>
+            <MdBusiness />
+            {company}
+          </p>
+        ) : null}
+        {location ? (
+          <p>
+            <MdLocationOn />
+            {location}
+          </p>
+        ) : null}
+        {blog ? (
+          <a href={`https://${blog}`}>
+            <MdLink /> {blog || 'A blog?'}
+          </a>
+        ) : null}
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,11 +6,11 @@ import { GithubContext } from '../context/context';
 const Dashboard = () => {
   return (
     <main>
-      <Navbar></Navbar>
-      <Search />
+      {/* <Navbar></Navbar> */}
+      {/* <Search /> */}
       <Info />
       <User />
-      <Repos />
+      {/* <Repos /> */}
     </main>
   );
 };
