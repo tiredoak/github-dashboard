fdd7a5e9a95ed780fe23c4a41535037047b991fc
Miguel Carvalho <miguelc@tuta.io>
2022-03-23 14:15:58 +0000

    Rendering info correctly with Item component

--- src/components/Info.js
+++ src/components/Info.js
@@ -39,11 +39,23 @@ const UserInfo = () => {
     },
   ];
 
+  const Item = ({ icon, label, value, color }) => {
+    return (
+      <article className='item'>
+        <span className={color}>{icon}</span>
+        <div>
+          <h3>{value}</h3>
+          <p>{label}</p>
+        </div>
+      </article>
+    );
+  };
+
   return (
     <section className='section'>
       <Wrapper className='section-center'>
         {items.map((item) => {
-          return <p>item</p>;
+          return <Item key={item.id} {...item} />;
         })}
       </Wrapper>
     </section>
