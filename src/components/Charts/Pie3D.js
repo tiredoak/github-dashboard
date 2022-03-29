bea259e8031eb9ea7a1db9b7ef895bce0f8e4e46
Miguel Carvalho <miguelc@tuta.io>
2022-03-29 15:53:37 +0100

    Changing chart styling to make it responsive

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -25,7 +25,7 @@ ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 const ExampleChart = ({ data }) => {
   const chartConfigs = {
     type: 'pie3d', // The chart type
-    width: '700', // Width of the chart
+    width: '100%', // Width of the chart
     height: '400', // Height of the chart
     dataFormat: 'json', // Data type
     dataSource: {
