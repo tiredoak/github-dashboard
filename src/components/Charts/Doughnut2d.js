5e7d903c0fa535fabb62f56ab7d29221e414406b
Miguel Carvalho <miguelc@tuta.io>
2022-03-31 09:54:50 +0100

    Removing comments and correcting chart data (axis names, captions)

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,43 +1,24 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from 'react';
-
-// Include the react-fusioncharts component
 import ReactFC from 'react-fusioncharts';
-
-// Include the fusioncharts library
 import FusionCharts from 'fusioncharts';
-
-// Include the chart type
 import Column2D from 'fusioncharts/fusioncharts.charts';
-
-// Include the theme as fusion
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
-// STEP 2 - Chart Data
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
 const Doughnut2D = ({ data }) => {
   const chartConfigs = {
-    type: 'doughnut2d', // The chart type
-    width: '100%', // Width of the chart
-    height: '400', // Height of the chart
-    dataFormat: 'json', // Data type
+    type: 'doughnut2d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
     dataSource: {
-      // Chart Configuration
       chart: {
         caption: 'Stars per Language',
         doughnutRadius: '45%',
         showPercentValues: 0,
         theme: 'candy',
       },
-      // Chart Data
-      // why wouldn't it read as {chartData} and data:chartData
       data,
     },
   };
--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,46 +1,23 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from 'react';
-
-// Include the react-fusioncharts component
 import ReactFC from 'react-fusioncharts';
-
-// Include the fusioncharts library
 import FusionCharts from 'fusioncharts';
-
-// Include the chart type
 import Column2D from 'fusioncharts/fusioncharts.charts';
-
-// Include the theme as fusion
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
-// STEP 2 - Chart Data
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
 const ExampleChart = ({ data }) => {
   const chartConfigs = {
-    type: 'pie3d', // The chart type
-    width: '100%', // Width of the chart
-    height: '400', // Height of the chart
-    dataFormat: 'json', // Data type
+    type: 'pie3d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
     dataSource: {
-      // Chart Configuration
       chart: {
         caption: 'Languages',
         theme: 'fusion',
-        subCaption: 'In MMbbl = One Million barrels',
-        xAxisName: 'Country',
-        yAxisName: 'Reserves (MMbbl)',
-        numberSuffix: 'K',
         pieRadius: '35%',
       },
-      // Chart Data
-      // why wouldn't it read as {chartData} and data:chartData
       data,
     },
   };
